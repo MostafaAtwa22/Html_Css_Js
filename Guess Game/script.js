0c529440756d55b267eef1565ca3f2ec1807f349
MostafaAtwa22 <mostafajobs22@gmail.com>
2025-08-12 18:48:21 +0300

    add features: Insert the inputs fields using js with dynamic number, and make it's style using css

--- Guess Game/script.js
+++ Guess Game/script.js
@@ -3,3 +3,36 @@ let gameName = "Guess the word";
 document.title = gameName;
 document.querySelector('h1').innerHTML = gameName;
 document.querySelector('footer').innerHTML = `${gameName} Create By Mostafa © 2025`;
+
+// game options
+let numberOfTries = 6;
+let numberOfInputs = 6;
+let currentTry = 1;
+
+function generateInput () {
+  let inputsDiv = document.querySelector('.inputs');
+
+  for (let i = 1; i <= numberOfInputs; i++) {
+  
+    let innerDiv = document.createElement('div');
+    innerDiv.classList.add(`try-${i}`);
+    innerDiv.innerHTML = `<span>Try ${i}</span>`;
+    
+    if (i !== 1) {
+      innerDiv.classList.add('disabled-input');
+    }
+
+    for (let j = 1; j <= numberOfInputs; j++) {
+      let input = document.createElement('input');
+      input.type = "text";
+      input.id = `guess-${i}-letter-${j}`;
+      input.maxLength = "1";
+      innerDiv.append(input);
+    }
+
+    inputsDiv.append(innerDiv);
+  }
+  inputsDiv.children[0].children[1].focus();
+}
+
+window.onload = generateInput;
