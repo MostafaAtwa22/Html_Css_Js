9014741da46a17d40b19679f03e115cc0ccfa490
MostafaAtwa22 <mostafajobs22@gmail.com>
2025-08-12 16:28:19 +0300

    first commit

---
+++ Guess Game/script.js
@@ -0,0 +1,5 @@
+// setggins game name
+let gameName = "Guess the word";
+document.title = gameName;
+document.querySelector('h1').innerHTML = gameName;
+document.querySelector('footer').innerHTML = `${gameName} Create By Mostafa © 2025`;
